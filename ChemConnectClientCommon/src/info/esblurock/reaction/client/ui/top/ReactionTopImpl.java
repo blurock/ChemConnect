4b36d77a98714ea00abdc35d1d265b776f36c951
edwardblurock <edward.blurock@gmail.com>
2017-04-02 16:30:57 +0200

    Update first page text

--- ChemConnectClientCommon/src/info/esblurock/reaction/client/ui/top/ReactionTopImpl.java
+++ ChemConnectClientCommon/src/info/esblurock/reaction/client/ui/top/ReactionTopImpl.java
@@ -36,7 +36,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Cookies;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
